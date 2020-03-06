db05945b1c03e513396680ce47cae98594f8593e
DimmaaaaaH <daemienho@gmail.com>
2020-03-06 12:03:21 +0100

    episode-2

--- tests/Feature/ReadThreadTest.php
+++ tests/Feature/ReadThreadTest.php
@@ -2,20 +2,26 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ReadThreadTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    use DatabaseMigrations;
+    /**   @test  */
+    public function a_user_can_view_all_threads()
     {
+        $thread = factory('App\Thread')->create();
         $response = $this->get('/threads');
-
-        $response->assertSee(200);
+        $response->assertSee($thread->title);
     }
+
+    public function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+        $response = $this->get('/threads/'. $thread->id);
+        $response->assertSee($thread->title);
+    }
+
 }
